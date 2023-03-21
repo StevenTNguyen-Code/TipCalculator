1d6fc1210a9eaae4b9fe78f08fd96d8a8423e6c2
Steven Nguyen <Steventt1@yahoo.com>
2023-03-21 12:41:02 -0700

    ContextMenu now appears for the 10% Button

--- app/src/main/java/com/zybooks/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/zybooks/tipcalculator/MainActivity.kt
@@ -45,6 +45,10 @@ class MainActivity : AppCompatActivity() {
         val listView3 = findViewById<RadioButton>(R.id.medium_radio_button)
         registerForContextMenu(listView3)
 
+        //This is the contextMenu for the 15% Button
+        val listView4 = findViewById<RadioButton>(R.id.light_radio_button)
+        registerForContextMenu(listView4)
+
 
         // Restore state
         if (savedInstanceState != null) {
